3a095c23bc0dc9c72885cfc077bf37310c763506
Abellarr <da.ortiz83@gmail.com>
2023-02-07 16:17:00 -0800

    Ready for presentation.

---
+++ index.js
@@ -0,0 +1,29 @@
+let btnLarge1 = document.getElementById('btnLarge1');
+btnLarge1.onclick = function() {
+    alert('No one told you to click the button!!');
+}
+
+let btnReg1 = document.getElementById('btnReg1');
+btnReg1.onclick = function() {
+    alert('No one told you to click the button!!');
+}
+
+let btnSmall1 = document.getElementById('btnSmall1');
+btnSmall1.onclick = function() {
+    alert('No one told you to click the button!!');
+}
+
+let btnGroup1 = document.getElementById('btnGroup1');
+btnGroup1.onclick = function() {
+    alert('I said this button does nothing!!');
+}
+
+let btnGroup2 = document.getElementById('btnGroup2');
+btnGroup2.onclick = function() {
+    alert('Don\'t you ever listen?!?!');
+}
+
+let btnGroup3 = document.getElementById('btnGroup3');
+btnGroup3.onclick = function() {
+    alert('Gah, I give up!! Do whatever you want!!');
+}
